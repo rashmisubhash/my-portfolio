35a04a38322de9a518997170ca8aeb74e7f6d5e7
Michasa <13708448+Michasa@users.noreply.github.com>
2024-11-02 19:15:51 +0000

    fix: :bug: fix mobile nav

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -23,7 +23,8 @@ function NavBar() {
 
   return (
     //h-42 for scroll ofset
-    <nav className="fixed right-0 top-3 z-10 flex h-12 w-fit flex-col items-end justify-center gap-y-3 px-4 font-blacker md:left-1/2 md:w-full md:-translate-x-1/2 md:flex-row md:justify-end md:gap-x-2 md:gap-y-0 lg:right-0 lg:max-w-screen-lg lg:p-0 lg:px-14">
+    <nav className="fixed right-0 top-3 z-10 h-1 w-fit items-end justify-center gap-y-3 bg-transparent px-4 font-blacker max-md:grid md:left-1/2 md:flex md:w-full md:-translate-x-1/2 md:flex-row md:justify-end md:gap-x-2 md:gap-y-0 lg:right-0 lg:max-w-screen-lg lg:p-0">
+      {" "}
       <button
         onClick={() => setIsOpen(!isOpen)}
         aria-label="menu"
@@ -44,7 +45,7 @@ function NavBar() {
           key={index}
           onClick={() => setIsOpen(false)}
           className={clsx(
-            "hover:outer-shadow-button combined-shadow-button relative w-32 text-nowrap rounded-full border border-indigo-950 bg-white px-4 py-2 text-center text-not-black transition-shadow hover:translate-y-1 hover:border-white/50 hover:bg-brand-green md:block",
+            "hover:outer-shadow-button combined-shadow-button relative text-nowrap rounded-full border border-indigo-950 bg-white px-4 py-2 text-center text-not-black transition-shadow hover:translate-y-1 hover:border-white/50 hover:bg-brand-green md:top-12 md:block",
             isOpen ? "block" : "hidden",
           )}
         >
